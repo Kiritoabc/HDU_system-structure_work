600c22b1d6ded6a833b11f977b64785090677381
Kirito <2493381254@qq.com>
2023-12-04 16:21:41 +0800

    update

--- hdu_springboot/src/main/java/org/hdu/hdu_springboot/controller/UserController.java
+++ hdu_springboot/src/main/java/org/hdu/hdu_springboot/controller/UserController.java
@@ -4,12 +4,14 @@ import org.hdu.hdu_springboot.mapper.UserMapper;
 import org.hdu.hdu_springboot.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
